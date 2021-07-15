f1aec4fd22851847228bf90ff7c9759f4407f787
KY <kunbam33@naver.com>
2021-07-15 18:35:21 +0900

    2nd

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 var server = http.createServer(app);
 
 app.get('/', function(req, res){
-    res.send('I like KY!');
+    res.send('I like KIMKY!');
 });
 
 app.get('/start', function(req, res){
